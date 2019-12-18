f97828a7c81b2f4ff32864b2a1166725470ee430
JUAN KIM <54922763+kookminjuan@users.noreply.github.com>
2019-12-18 20:30:10 +0900

    Add files via upload

--- FeelMore/ex.py
+++ FeelMore/ex.py
@@ -74,90 +74,6 @@ class Functions(QWidget):
             layout.addLayout(main_layout)
             self.setLayout(layout)
 
-        elif sender == "영작 연습하기":
-            main_layout = QVBoxLayout()
-            button_layout = QHBoxLayout()
-            self.draft_input = QTextEdit()
-            self.korean_input = QLineEdit()
-            self.copy_button = QPushButton("클립보드에 복사")
-            self.to_eng_button = QPushButton("영어로")
-            self.read_button = QPushButton("읽어주기")
-            self.copy_button.clicked.connect(self.copy)
-            self.to_eng_button.clicked.connect(self.korToEng)
-            self.korean_input.setFixedWidth(300)
-            main_layout.addStretch(1)
-            main_layout.addWidget(self.draft_input)
-            button_layout.addWidget(self.copy_button)
-            button_layout.addStretch(1)
-            button_layout.addWidget(self.korean_input)
-            button_layout.addWidget(self.to_eng_button)
-            button_layout.addWidget(self.read_button)
-            main_layout.addLayout(button_layout)
-            main_layout.addStretch(1)
-            layout.addLayout(main_layout) # self.setLayout(main_layout)
-            self.setLayout(layout)
-
-    def copy(self):
-        pyperclip.copy(self.draft_input.toPlainText())
-
-    def korToEng(self):
-        translated = web.papago(self.korean_input.text()[:300], True)
-        self.draft_input.setText(self.draft_input.toPlainText() + " " + translated)
-
-
-    def play_music(self, music_file, volume=0.8):
-        '''
-        stream music with mixer.music module in a blocking manner
-        this will stream the sound from disk while playing
-        '''
-        # set up the mixer
-        freq = 24100  # audio CD quality
-        bitsize = -16  # unsigned 16 bit
-        channels = 1  # 1 is mono, 2 is stereo
-        buffer = 1000  # number of samples (experiment to get best sound)
-        pg.mixer.init(freq, bitsize, channels, buffer)
-        # volume value 0.0 to 1.0
-        pg.mixer.music.set_volume(volume)
-        clock = pg.time.Clock()
-        try:
-            pg.mixer.music.load(music_file)
-        except pg.error:
-            print("File {} not found! ({})".format(music_file, pg.get_error()))
-            return
-        pg.mixer.music.play()
-        while pg.mixer.music.get_busy():
-            # check if playback has finished
-            clock.tick(30)
-
-    def buttonClicked(self):
-        button = self.sender()
-        word = button.text()
-        self.eng_display.setText("English: " + word)
-        self.kor_display.setText("Korean: "+ web.papago(word))
-        if self.country.currentText() == "US":
-            us = gTTS(text=word, lang='en-us')
-            us_file = "us.mp3"
-            us.save(us_file)
-            music_file = us_file
-            volume = 0.8
-            self.play_music(music_file, volume)
-        elif self.country.currentText() == "UK":
-            uk = gTTS(text=word, lang='en-uk')
-            uk_file = "uk.mp3"
-            uk.save(uk_file)
-            music_file = uk_file
-            volume = 0.8
-            self.play_music(music_file, volume)
-        else:
-            za = gTTS(text=word, lang='en-ca')
-            za_file = "ca.mp3"
-            za.save(za_file)
-            # print("convert complete")
-            music_file = za_file
-        # optional volume 0 to 1.0
-            volume = 0.8
-            self.play_music(music_file, volume)
-
 class Main(QMainWindow):
     def __init__(self, parent=None):
         super(Main, self).__init__(parent)
@@ -179,18 +95,16 @@ class Main(QMainWindow):
         sender = self.sender().text()
         print(sender)
         if sender == menuOrder[0] or sender == "단어 섞기":
-            print('a')
             self.window = Vocabulary()
+            self.window.shuffle_btn.clicked.connect(self.useFunctions)
         elif sender == menuOrder[1]:
             print("d")
         elif sender == menuOrder[2]:
             self.window = Writing()
         self.setCentralWidget(self.window)
         self.window.exit_button.clicked.connect(self.startMenu)
-        try:
-            self.window.shuffle_btn.clicked.connect(self.useFunctions)
-        except:
-            pass
+
+
         self.show()
 
 if __name__ == '__main__':
